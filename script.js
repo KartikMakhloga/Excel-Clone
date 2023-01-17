6277132b62d370ced015df51026a41a19407e626
kartik <kartikmakhloga20@gmail.com>
2023-01-17 17:34:16 +0530

    update grid section completely

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let body = document.querySelector("body");
 body.spellcheck = false;
+let columnTags = document.querySelector(".column-tags");
+let rowNumbers = document.querySelector(".row-numbers");
+let grid = document.querySelector(".grid");
+let oldCell;
+let formulaSelectCell = document.querySelector("#select-cell");
 
 let menuBarPtags = document.querySelectorAll(".menu-bar p");
 
@@ -15,4 +20,43 @@ for (let i = 0; i < menuBarPtags.length; i++) {
       e.currentTarget.classList.add("menu-bar-option-selected");
     }
   });
+}
+
+for (let i = 0; i < 26; i++) {
+  let div = document.createElement("div");
+  div.classList.add("column-tag-cell");
+  div.innerText = String.fromCharCode(65 + i);
+  columnTags.append(div);
+}
+
+for (let i = 1; i <= 100; i++) {
+  let div = document.createElement("div");
+  div.classList.add("row-number-cell");
+  div.innerText = i;
+  rowNumbers.append(div);
+}
+
+for (let j = 1; j <= 100; j++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  for (let i = 0; i < 26; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.setAttribute("data-address", String.fromCharCode(i + 65) + j);
+
+    cell.addEventListener("click", function (e) {
+      //checking any cell selected already or not
+      if (oldCell) {
+        //if yes than deselect it
+        oldCell.classList.remove("grid-selected-cell");
+      }
+      e.currentTarget.classList.add("grid-selected-cell");
+      oldCell = e.currentTarget;
+      let cellAddress = e.currentTarget.getAttribute("data-address");
+      formulaSelectCell.value = cellAddress;
+    });
+    cell.contentEditable = true;
+    row.append(cell);
+  }
+  grid.append(row);
 }

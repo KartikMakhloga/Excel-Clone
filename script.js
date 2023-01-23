2517e3382da7247b8aa4f9b37959a52cd3c30dc8
kartik <kartikmakhloga20@gmail.com>
2023-01-23 14:13:45 +0530

    added notes for cells

--- script.js
+++ script.js
@@ -61,9 +61,52 @@ for (let j = 1; j <= 100; j++) {
   grid.append(row);
 }
 
+// NOTES
+//  (1*)
 // How can a cell have a value?
-//   1- Direct input (not dependent on any other cell)
-//   2- formula (dependent on cells which are present in formula)
+//   A- Direct input (not dependent on any other cell)
+//   B- formula (dependent on cells which are present in formula)
 //    for eg-> C1 = A1 + B1
 
 //    here, C1 is dependent party and A1,B1 is not dependent party
+
+
+// (2*)
+// A cell can have four arrays possible
+// A- value[]
+// B- formuls[]
+// C- downStream[ jo cell humpar dependent hai ]
+// D- upStream[ jis cell par hum dependent hai ]
+
+// we have four cases (a) di -> di 
+//                    (b) di -> formula 
+//                    (c) f -> di
+//                    (d) f -> f
+
+// Case 1:    di -> di [worst case]
+           
+//            value: 50  se  60 hogya yaha pe
+//            formula: ""  [no change]
+//            upstream: [no change]
+//            downstream: [A1,B1,C1] teeno update honge agar value array change hogi
+
+// case 2:     di -> f  [worst case]    [Z6 + D9]  Z6=20, D9=50
+            
+//            value: 20 se 70 hojayega
+//            formula: "" se "Z6 + D9" hojayega
+//            upstream: empty se [Z6,D9]
+//            downstream: [A1,B1,C1] teeno update honge 
+
+// case 3:      f -> f  [worst case]  C1 -> 2 * D2
+
+//             value: 30 se update hojayega
+//             formula: "3 * A1" se "2 * D2" hojayega
+//             upstream: [A1] se [D2] and D2 downstream mei khudko daalna padega or A2 ki downstream se remove kardenge
+//             downstream: [B1] update honga
+
+// case 4:      f -> di  [worst case]  
+
+//             value: 20 se 30 update karenge
+//             formula: "2 * B1" se "" hojayega
+//             upstream: [B1] se [empty] hojayega lekin usse pehle B1 ki downstream se apne aap ko remove kr denge 
+//             downstream: [D1,E1] update honge
